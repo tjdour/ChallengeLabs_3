5d1dfbdfa8a4087c5e2d4253c98e4b91f80e38c8
tjdour <tongjdour@outlook.com>
2026-07-21 18:10:08 +0300

    Add project files.

---
+++ ChallengeLab_3.1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a word to check if Palindrome: ");
+        string pal = Console.ReadLine();
+
+        Console.WriteLine($"is {pal} a palindrome? {isPal(pal)}");
+
+        bool isPal(string pal)
+        {
+
+            int left = 0;
+            int right = pal.Length - 1;
+
+            while (left < right)
+            {
+
+                if (pal[left] != pal[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+
+            return true;
+
+       }
+    }
+}
+ 
