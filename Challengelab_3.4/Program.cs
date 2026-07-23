a29110ee5e9ad8c9538f329708025a0a90ae74ec
tjdour <tongjdour@outlook.com>
2026-07-23 23:08:36 +0300

    Challenge lab 3.4

---
+++ Challengelab_3.4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Challenge Lab 3.4
+
+Console.WriteLine("Input string: ");
+string s = Console.ReadLine().ToUpper();
+
+int result = MinStringLegth(s);
+Console.WriteLine($"Output: { result}");
+
+int MinStringLegth (string s) {
+    while (s.Contains("AB") || s.Contains("CD"))
+    {
+        s = s.Replace("AB", "");
+        s = s.Replace("CD", "");
+    }
+    
+    return s.Length;
+}
